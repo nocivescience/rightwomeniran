8cf1a47446c9da7fe0c88b057b26d5481d0266ff
noceive <nocivescience@gmail.com>
2023-04-05 15:03:04 -0400

    un poco mejor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,15 +21,10 @@ export const Navbar = function () {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-              <NavLink className="nav-link" to='/form'>
-                Form
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to='/gallery'>
-                Gallery
-              </NavLink>
-            </li>
+            <NavLink className="nav-link" to='/gallery'>
+              Gallery
+            </NavLink>
+          </li>
           </ul>
           <div className="d-flex">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -60,25 +55,8 @@ export const Navbar = function () {
                 <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
               </svg></a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Signin</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="{% url 'signup' %}">Signup</a>
-            </li>
           </ul>
         </div>
-          <form className="d-flex">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-secondary" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
